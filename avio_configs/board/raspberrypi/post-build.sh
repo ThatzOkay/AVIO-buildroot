5fe6c8fe47dc252953d06b9b31217cb8cdf11f93
Kayori <noduhgamingxd@gmail.com>
2026-07-22 15:11:59 +0200

    Update Raspberry Pi configuration for AVIO kiosk setup and add init script

--- avio_configs/board/raspberrypi/post-build.sh
+++ avio_configs/board/raspberrypi/post-build.sh
@@ -4,4 +4,20 @@ set -u
 set -e
 set -x
 
+TMP=$(mktemp -d)
+
+LATEST=$(curl -s https://api.github.com/repos/ThatzOkay/AVIO/releases \
+  | jq -r '.[0].assets[] | select(.name | endswith(".deb") and contains("arm64")) | .browser_download_url')
+
+curl -L -o "$TMP/avio.deb" "$LATEST"
+
+cd "$TMP"
+ar x avio.deb
+tar -xf data.tar.xz
+
+install -Dm755 usr/bin/avio /usr/bin/avio
+cp -r usr/share/avio /usr/share/
+
+rm -rf "$TMP"
+
 source board/raspberrypi/post-build.sh
